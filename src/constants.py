96175a1b84addacd9dc38f22d3d702c68b8bf7c7
Wanghley Soares Martins <wanghleys@gmail.com>
2024-12-08 15:00:49 -0500

    refactor: remove not used constants

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,8 @@
 import os
 import numpy as np
-from enum import Enum
 
 TRAIN_FILE = os.path.join('../data/Train_Arabic_Digit.txt')
 TEST_FILE = os.path.join('../data/Test_Arabic_Digit.txt')
-DF_FILE = os.path.join('../data/dataframe.csv')
 
 NUM_MFCC = 13
 ALL_COEFFS = np.arange(0, NUM_MFCC, 1)
